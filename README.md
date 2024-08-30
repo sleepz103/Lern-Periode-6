1947fe577e1ee6d5e961be0f46840a8782cf2ac5
sleepz1 <98420917+sleepz103@users.noreply.github.com>
2024-08-30 17:18:46 +0200

    Cesär Ziffer

--- README.md
+++ README.md
@@ -12,11 +12,7 @@
 
 - [ ] Falls Zeit übrig ist, Shift Chiffre Programm fertigen, indem dort ein Wert nicht mehr unten null gehen kann.
 
-
-
 Heute habe ich den Unterschied zwischen Eigenschaften und Feldern verstanden. Das ist weil ich zwei Videos angeschaut habe. Nun konnte ich mir das notieren. Ich habe danach zu Auftrag gewächselt. Der Auftrag ist nicht wirklich kompliziert, eher das übersetzen der Ideen in Eigenschaften und Methoden. Ich weiss noch nicht, wie Methode funktionier und deswegen ist es schlecht gegangen. (58)
-
-
 
 ## 30/08
 
@@ -25,3 +21,9 @@ Heute habe ich den Unterschied zwischen Eigenschaften und Feldern verstanden. Da
 - [x] Erstelle 10 Fragen für Checkliste und überprüfe kanti baden Webseite
 
 - [x] Falls Zeit übrig ist, Shift Chiffre Programm fertigen, indem dort ein Wert nicht mehr unten null gehen kann.
+
+Heute habe ich am Anfang an zwei Aufträge gearbeitet. Da ich seit letztes Lernperiode gelernt habe, wie Methoden funktionieren, konnte ich den Auftrag leicht lösen. Danach habe ich das Auftrag über Accessibility mit Robin aufgeteilt, sodass wir schneller den Auftrag erfüllen. Gegen ende habe ich eine Idee für den Schifr entworfen. Es hat tatsächlich funktioniert, und nun funktioniert der erste Teil des Shift Ziffer, also ein Cesär Ziffer. Die Arbeit war nicht so schwierig heute, habe aber auch leicht gearbeitet. (80)
+
+## 06/08
+
+- [ ] 
---
+++ Shift Cipher/Program.cs
@@ -0,0 +1,90 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Shift_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int shift = 36;
+            char letterCiphered;
+            char letterDeciphered;
+            int letterCipheredIndexNum;
+            int letterDecipheredIndexNum;
+
+
+            
+            //char[] cipheredMessage = "IZW WOZNCCM FSWHLJX UILR SMHGT WVZXH! DYE DQEVG PMHN HCO NLE TSBUZFLUHA GYGR NKW MMTLV!".ToCharArray();
+            //char[] cipheredMessage = "E".ToCharArray();
+            char[] cipheredMessage = "DRSC SC K MOCOKB MSZROB KXN SDC UOI SC 36!".ToCharArray();
+
+            string decipheredMessage = "";
+
+            for (int i = 0; i < cipheredMessage.Length; i++)
+            {
+                letterCiphered = cipheredMessage[i];
+
+                //decipher letters only
+                if (Char.IsLetter(letterCiphered))
+                {
+                    //shift cannot be big
+                    shift = shift % 26;
+                    letterCipheredIndexNum = FindLetterIndex(letterCiphered);
+                    letterDecipheredIndexNum = letterCipheredIndexNum - shift;
+                    if (letterDecipheredIndexNum < 0 )
+                    {
+                        letterDecipheredIndexNum = letterDecipheredIndexNum + 26;
+                    }
+                    letterDeciphered = FindLetterByIndex(letterDecipheredIndexNum);
+                    decipheredMessage += letterDeciphered.ToString();
+                }
+                else
+                {
+                    letterDeciphered = letterCiphered;
+                    decipheredMessage += letterDeciphered.ToString();
+                }
+            }
+
+            Console.WriteLine(decipheredMessage);
+            
+        }
+
+        static int FindLetterIndex(char letter)
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            int letterIndex = 0;
+
+            for (int i = 0; i < alphabet.Length; i++ )
+            {
+                if (alphabet[i] == letter)
+                {
+                    letterIndex = i;
+                    break;
+                }
+
+            }
+
+            return letterIndex;
+        }
+
+        static char FindLetterByIndex(int letterIndexNum)
+        {
+            char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char letterByIndex = ' ';
+
+            for (int i = 0; i < alphabet.Length; i++)
+            {
+                if (i == letterIndexNum)
+                {
+                    letterByIndex = alphabet[i];
+                    break;
+                }
+
+            }
+
+            return letterByIndex;
+        }
+
+    }
+}
